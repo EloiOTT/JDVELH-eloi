a579d51472214d0995dc8b2e0d260cdaf5c7ad19
EloiOTT <eloi.chambrun@gmail.com>
2020-02-03 15:17:48 +0100

    choix par numéro

---
+++ JDVELH.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  int deplacement ;
+
+  printf("début trajet \n [1] choix 1 \n [2] choix 2");
+  scanf("%d", &deplacement);
+
+    if (deplacement == 1){
+      printf("arrivé à déstination1 \n")
+  }
+
+    if (deplacement == 2){
+      printf("arrivé à déstination2 \n")
+  }
